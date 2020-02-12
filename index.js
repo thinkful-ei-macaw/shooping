5e04a1c2e341fc784794a7c0add1d99ca62f0004
Dasha Akhten <dasha.akhten@gmail.com>
2020-02-12 14:20:11 -0800

    notes added

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (listItem !== "") {
 
 
 $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-$(this).closest('.shopping-item-controls').siblings('shopping-item').toggleClass('shopping-item__checked');
+$(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
 });
 
 $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
@@ -49,3 +49,66 @@ $(shoppingList);
 
 
 
+/*$('submit.button').click(function(event) {
+      $("ul").append('<li><a href="#">New list item</a></li>');
+        
+}
+
+$(shoppingList);
+
+
+
+  /*$( "#other" ).click(function() {
+    $( "#target" ).submit();
+  });*/
+
+  /*$('.items').on('click', '.delete-button', event => {
+    
+$('ul').on('click', 'li', function(event) {
+    this.delete();
+    console.log(removed)
+
+});
+
+/*function shoppingList() {
+  
+  
+    $('#js-shopping-list-form').submit(function(event){
+      event.preventDefault();
+      
+      const listItem = $('.js-shopping-list-entry').val();
+      
+      
+     if (listItem !== "") {
+        $('.shopping-list').append(`<li>
+          <span class="shopping-item">${listItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+        <li>`);
+        $('.js-shopping-list-entry').val("");
+    }
+  });
+  
+  
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  
+  
+  
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+  }
+   
+  
+  
+  
+  $(shoppingList);
